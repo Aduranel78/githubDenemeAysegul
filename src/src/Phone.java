7130321b127328e6b4cd8b6497d8ec49b1c55937
Aduranel78 <56616738+Aduranel78@users.noreply.github.com>
2019-11-17 23:24:26 -0500

    add phone with 3 methods

---
+++ src/src/Phone.java
@@ -0,0 +1,18 @@
+public class Phone {
+    public String color;
+    public int age;
+    public String serialnumber;
+    public boolean isnew;
+    public byte storage;
+
+
+    public void cdwa(){
+
+    }
+    public void gsm(){
+
+    }
+    public void throwable(){
+
+    }
+}
